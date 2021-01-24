5ce4f46b800614d3e2a26ed7a650a555af45f26b
marianabeldi <mbeldi@gmail.com>
2021-01-24 19:01:52 -0300

    posts

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,4 +17,12 @@ export default {
   content: {
     fullTextSearchFields: ['title', 'description', 'slug', 'text'],
   },
+  generate: {
+    async routes () {
+      const { $content } = require('@nuxt/content')
+      const files = await $content({ deep: true }).only(['path']).fetch()
+
+      return files.map(file => file.path === '/index' ? '/' : file.path)
+    }
+  }
 }
