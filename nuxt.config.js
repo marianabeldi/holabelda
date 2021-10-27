2ed56f6bee750a395f3200c9f7279ec2fb8b5938
marianabeldi <mbeldi@gmail.com>
2021-10-27 09:44:18 -0300

    build

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,6 @@ export default {
     async routes () {
       const { $content } = require('@nuxt/content')
       const files = await $content({ deep: true }).only(['path']).fetch()
-
       return files.map(file => file.path === '/index' ? '/' : file.path)
     }
   },
